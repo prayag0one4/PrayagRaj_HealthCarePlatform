b4de1a6259e121bc7e0b1db20282140614781973
anishka7755 <anishkaverma046@gmail.com>
2024-11-10 03:32:33 +0530

    styled consultation button

--- 01project/src/pages/Doctors/SidePanel.jsx
+++ 01project/src/pages/Doctors/SidePanel.jsx
@@ -34,7 +34,7 @@ const SidePanel = () => {
       <button className="w-full mt-6 bg-blue-600 text-white py-3 rounded-md hover:bg-blue-700 transition-colors font-medium">
         Book Appointment
       </button>
-      <Videocallbutton/>
+      <Videocallbutton />
     </div>
   );
 };
--- 01project/src/pages/Doctors/videocallbutton.jsx
+++ 01project/src/pages/Doctors/videocallbutton.jsx
@@ -1,14 +1,17 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Videocallbutton = () => {
   const navigate = useNavigate();
 
   const handleRedirect = () => {
-    navigate('/video-call');
+    navigate("/video-call");
   };
 
   return (
-    <button onClick={handleRedirect}>
+    <button
+      onClick={handleRedirect}
+      className="w-full mt-6 bg-blue-200 text-black py-3 rounded-md shadow-md hover:shadow-lg transition-shadow font-medium"
+    >
       Start Video Consultation
     </button>
   );
