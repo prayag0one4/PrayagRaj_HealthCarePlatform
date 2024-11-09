37f5f502dc5aadb87898a0a2e6a7984acb369ff0
anishka7755 <anishkaverma046@gmail.com>
2024-11-10 03:19:24 +0530

    linked pages

--- 01project/src/components/Header.jsx
+++ 01project/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router-dom";
 import { BiMenu } from "react-icons/bi";
 import { useEffect, useRef } from "react";
 import { Auth0Provider, useAuth0 } from "@auth0/auth0-react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 const Header = () => {
   const { t } = useTranslation();
@@ -13,30 +13,30 @@ const Header = () => {
   const navLinks = [
     {
       path: "/home",
-      display: t('dash1'), // Translated text
+      display: t("dash1"), // Translated text
     },
     {
       path: "/doctors",
-      display: t('dash3'), // Translated text
+      display: t("dash3"), // Translated text
     },
     {
       path: "/pharmacy",
-      display: t('dash4'), // Translated text
+      display: t("dash4"), // Translated text
     },
     {
       path: "/healtheducation",
-      display: t('dash5'), // Translated text
+      display: t("dash5"), // Translated text
     },
     {
       path: "/image",
-      display: t('dash6'), // Translated text
+      display: t("dash6"), // Translated text
     },
     {
       path: "/contact",
-      display: t('dash7'), // Translated text
+      display: t("dash7"), // Translated text
     },
   ];
-  const{user,loginWithRedirect,isAuthenticated,logout} =  useAuth0();
+  const { user, loginWithRedirect, isAuthenticated, logout } = useAuth0();
 
   const handleStickyHeader = () => {
     window.addEventListener("scroll", () => {
@@ -55,7 +55,7 @@ const Header = () => {
     handleStickyHeader();
     return () => window.removeEventListener("scroll", handleStickyHeader);
   }, []);
-  
+
   const toggleMenu = () => menuRef.current.classList.toggle("shown__menu");
 
   return (
@@ -63,7 +63,9 @@ const Header = () => {
       <div className="w-60 h-screen bg-gray-50 fixed left-0 top-0">
         {/* Logo */}
         <div className="p-4">
-          <h1 className="text-indigo-600 text-xl font-semibold">{t('title')}</h1>
+          <h1 className="text-indigo-600 text-xl font-semibold">
+            {t("title")}
+          </h1>
         </div>
 
         {/* Menu */}
@@ -95,12 +97,9 @@ const Header = () => {
             </Link>
           </div>
         </div>
-        <Link to="login">
-          <button className=" bg-blue-500 py-2 px-6 text-white hover:bg-blue-600 font-[600] h-[44px] flex items-center justify-center rounded-[50px]   ml-2 mt-10">
-            Login
-          </button>
-        </Link>
-        
+        <button className=" bg-blue-500 py-2 px-6 text-white hover:bg-blue-600 font-[600] h-[44px] flex items-center justify-center rounded-[50px]   ml-2 mt-10">
+          Logout
+        </button>
         <span className="md:hidden" onClick={toggleMenu}>
           <BiMenu className="w-6 h-6 cursor-pointer" />
         </span>
